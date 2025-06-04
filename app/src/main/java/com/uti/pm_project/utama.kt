9ca8dac5d15e7bfc241b6d99171cbf3c86f36a2c
Reva Ekalia <reva_ekalia@teknokrat.ac.id>
2025-06-04 22:59:05 +0700

    commit

---
+++ app/src/main/java/com/uti/pm_project/utama.kt
@@ -0,0 +1,14 @@
+package com.uti.pm_project
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class utama: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_utama)
+    }
+}
